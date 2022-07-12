d08f7f59e546f22e1b967cf0568e85a83c1b26fd
sf <168618014@qq.com>
2022-07-12 12:39:40 +0800

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p v-for="item in messages" :key="item">{{ item }}</p>
+  </div>
+</template>
+<script setup>
+// import { ref } from "vue";
+// export default defineComponent()
+//  export default{
+//   name: 'AboutView',
+//   setup(){
+//     return {
+//       messages
+//     }
+//   }
+// }
+
+const messages = ["qwe", "asd", "zxc"];
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
